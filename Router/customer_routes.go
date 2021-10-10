17d10b361ff15b965c884afc9eb745a3310705c3
YING-ZR <68770431+YING-ZR@users.noreply.github.com>
2021-10-10 15:01:36 +0800

    Add files via upload

--- Router/customer_routes.go
+++ Router/customer_routes.go
@@ -8,5 +8,11 @@ import (
 func CollocetRouter(r *gin.Engine) *gin.Engine {
 	r.POST("/repair/customer/register", Controller.Register)
 	r.POST("/repair/customer/login", Controller.Customer_Login)
+	r.PUT("/repair/customer/modify",Controller.Customer_modify)
+	r.POST("/repair/customer/set_table", Controller.Set_Table)
+	r.GET("/repair/customer/gettable", Controller.GetTable)
+	r.GET("/repair/customer/get_table", Controller.Get_Table)
+	r.DELETE("/repair/customer/delete_table",Controller.Delete_Table)
+	r.PUT("/repair/customer/update_table",Controller.Update_Table)
 	return r
 }
